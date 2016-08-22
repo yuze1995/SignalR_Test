0e175228c899c7df5718e395dcbc38def902f8c8
User <User@USER-PC>
2016-08-22 17:05:02 +0800

    加入專案檔案。

---
+++ SignalR_Test/Hub/ChatHub_All.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace SignalR_Test
+{
+    public class ChatHub_All : Hub
+    {
+        public void HelloToAll(string name)
+        {
+            Clients.All.addNewMember(name);
+        }
+
+        public void SendMessageToAll(string name, string msg)
+        {
+            Clients.All.AddMessage(name, msg);
+        }
+    }
+}
---
+++ SignalR_Test/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SignalR_Test.Startup))]
+namespace SignalR_Test
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
